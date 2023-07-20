823e29b7454e735c7c6f64f63bdf6821d61894ad
Prashant Goswami <87839549+prashantcoding@users.noreply.github.com>
2023-07-20 22:21:37 +0530

    Added solution - LeetHub

--- Modular Exponentiation for large numbers - GFG/modular-exponentiation-for-large-numbers.java
+++ Modular Exponentiation for large numbers - GFG/modular-exponentiation-for-large-numbers.java
@@ -29,23 +29,21 @@ class GFG
 
 //User function Template for Java
 
-
 class Solution
 {
     public long PowMod(long x, long n, long m)
     {
         long res=1;
-        x=x%m;
-        if(x==0){
-            return 0;
-        }
+        if(x%m==0) return 0;
+        
         else{
             while(n>0){
                 if((n&1)!=0){
-                res=(res*x)%m;
-            }
-            n=n>>1;
-            x=(x*x)%m;
+                    res=res*x%m;
+                }
+                n=n>>1;
+                x=(x*x)%m;
+                
             }
         }
         return res;
