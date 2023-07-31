88e766334b5345e0b7c44496870429a5f5b80fa7
Prashant Goswami <87839549+prashantcoding@users.noreply.github.com>
2023-07-31 23:33:47 +0530

    Added solution - LeetHub

---
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.java
@@ -0,0 +1,83 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class gfg
+{
+    public static void main(String args[])throws IOException
+    {
+        //reading input using BufferedReader class
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        
+        //reading total testcases
+        int t = Integer.parseInt(read.readLine());
+        
+        while(t-- > 0)
+        {
+            //reading number of elements and weight
+            int n = Integer.parseInt(read.readLine());
+            int w = Integer.parseInt(read.readLine());
+            
+            int val[] = new int[n];
+            int wt[] = new int[n];
+            
+            String st[] = read.readLine().trim().split("\\s+");
+            
+            //inserting the values
+            for(int i = 0; i < n; i++)
+              val[i] = Integer.parseInt(st[i]);
+             
+            String s[] = read.readLine().trim().split("\\s+"); 
+            
+            //inserting the weigths
+            for(int i = 0; i < n; i++)
+              wt[i] = Integer.parseInt(s[i]);
+              
+            //calling method knapSack() of class Knapsack
+            System.out.println(new Solution().knapSack(w, wt, val, n));
+        }
+    }
+}
+
+
+
+
+// } Driver Code Ends
+
+
+class Solution 
+{ 
+    
+    //Function to return max value that can be put in knapsack of capacity W.
+    static int knapSack(int W, int wt[], int val[], int n) 
+    { 
+        int [][]dp=new int[n+1][W+1];
+        for(int []x:dp){
+            Arrays.fill(x,-1);
+        }
+       return knapSackM(W,wt,val,n,dp);
+       
+    } 
+    
+     static int knapSackM(int W, int wt[], int val[], int n,int[][]dp) 
+    { 
+        if(n==0||W==0) return 0;
+        
+        if( dp[n][W] !=-1 ) return dp[n][W];
+        if(W>=wt[n-1]){
+            return dp[n][W]=Math.max(val[n-1]+knapSackM(W-wt[n-1],wt,val,n-1,dp),
+                knapSackM(W,wt,val,n-1,dp));
+        
+        }
+        else{
+          return  dp[n][W]=knapSackM(W,wt,val,n-1,dp);
+        }
+    } 
+    
+    
+}
+
+
+
+
